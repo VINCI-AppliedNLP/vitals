dc252b7a2d80b78e01ad6bd9691144e07ca33653
rcornia <11climb06>
2016-12-02 15:09:45 -0600

    fix for update to leo that wasn't catching children annotations.
    Former-commit-id: 4dd99b8df0bafece4596ae12d37bab69c68b2765

--- src/main/java/gov/va/vinci/vitals/ae/ExtractRespiratoryAE.java
+++ src/main/java/gov/va/vinci/vitals/ae/ExtractRespiratoryAE.java
@@ -53,7 +53,7 @@ public class ExtractRespiratoryAE extends BaseVitalExtractorAE {
 			Unit curUnit = null;
 			if (coverWindow.size() > 0 && potentialBps.size() == 0) {
 				Annotation currWindow = (coverWindow.get(0));
-				if (AnnotationLibrarian.getAllOverlappingAnnotationsOfType(currWindow, Numeric.type, false).size() > 4) {
+				if (AnnotationLibrarian.getAllOverlappingAnnotationsOfType(currWindow, Numeric.type, true).size() > 4) {
 					processValue(value, currentType, curUnit, false, typeRanges);
 				}
 			} // end of double number loop
